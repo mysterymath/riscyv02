ebcf290bc9f56695ae678a16a9c1c004cd9fbb53
Daniel Thornburgh <mysterymath@gmail.com>
2024-02-24 15:26:23 -0800

    Register file implementation

--- rv8.v
+++ rv8.v
@@ -1,18 +1,25 @@
-module rf(clk, r_num, w_num, w_en, r, w);
+module rf(clk, r_num, w_num, w_en, w, r);
 input clk;
 input [2:0] r_num;
 input [2:0] w_num;
-input [15:0] r;
+input w_en;
 input [15:0] w;
 
-reg [15:0] regs [2:0];
+output reg [15:0] r;
 
-assign r = r_num == 3'b0 ? 15'b0 : regs[r_num];
+reg [15:0] regs [6:0];
 
-always @(posedge clock) begin
-  if (w_en) begin
-    regs[w_num] <= w;
-  end
+always @(posedge clk) begin
+  if (w_en && w_num != 3'b0) begin
+    regs[w_num-1] <= w;
+    if (r_num == 3'b0)
+      r <= 16'b0;
+    else if (r_num == w_num)
+      r <= w;
+    else
+      r <= regs[r_num-1];
+  end else
+    r <= r_num == 3'b0 ? 16'b0 : regs[r_num-1];
 end
 
 endmodule
