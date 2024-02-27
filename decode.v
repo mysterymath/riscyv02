ddddce2b0d5c2bbb135de006f0f4ddc1455c7c7a
Daniel Thornburgh <mysterymath@gmail.com>
2024-02-26 21:45:44 -0800

    More work

--- decode.v
+++ decode.v
@@ -17,39 +17,40 @@ input [15:0] rf_r;
 
 reg [2:0] rs2_num;
 
-parameter [2:0] LUI   = 3'b000;
-parameter [2:0] AUIPC = 3'b001;
-parameter [2:0] JAL   = 3'b010;
-parameter [3:0] BNZ   = 4'b0010;
-parameter [4:0] ADDI  = 5'b00100;
-parameter [4:0] SLTI  = 5'b00101;
-parameter [4:0] SLTIU = 5'b00110;
-parameter [4:0] ANDI  = 5'b00111;
-parameter [4:0] ORI   = 5'b01011;
-parameter [4:0] XORI  = 5'b01100;
-parameter [4:0] SLI   = 5'b01101;
-parameter [4:0] SRI   = 5'b01110;
-parameter [4:0] JALR  = 5'b01111;
-parameter [4:0] LB    = 5'b10100;
-parameter [4:0] LBU   = 5'b10101;
-parameter [4:0] LW    = 5'b10110;
-parameter [4:0] SB    = 5'b10111;
-parameter [4:0] SW    = 5'b11011;
-parameter [6:0] ADD   = 7'b0011100;
-parameter [6:0] SEQ   = 7'b0011101;
-parameter [6:0] SNE   = 7'b0011110;
-parameter [6:0] SLT   = 7'b0011111;
-parameter [6:0] SLTU  = 7'b0111100;
-parameter [6:0] SGE   = 7'b0111101;
-parameter [6:0] SGEU  = 7'b0111110;
-parameter [6:0] AND   = 7'b0111111;
-parameter [6:0] OR    = 7'b1011100;
-parameter [6:0] XOR   = 7'b1011101;
-parameter [6:0] SLL   = 7'b1011110;
-parameter [6:0] SRL   = 7'b1011111;
-parameter [6:0] SUB   = 7'b1111100;
-parameter [6:0] SRA   = 7'b1111101;
-parameter [6:0] INT   = 7'b1111110;
+parameter TRAP  = 5'b00000;
+parameter MOVI  = 5'b00001;
+parameter ADDI  = 5'b00010;
+parameter ANDI  = 5'b00011;
+parameter ORI   = 5'b00100;
+parameter XORI  = 5'b00101;
+parameter SLI   = 5'b00110;
+parameter SRI   = 5'b00111;
+parameter JALR  = 5'b01000;
+parameter SLTI  = 5'b01001;
+parameter SLTIU = 5'b01010;
+parameter LUI   = 5'b01011;
+parameter AUIPC = 5'b01100;
+parameter BZ    = 5'b01101;
+parameter BNZ   = 5'b01110;
+parameter JAL   = 5'b01111;
+parameter INT   = 5'b10000;
+
+parameter LB    = 5'b10001;
+parameter LBU   = 5'b10010;
+parameter LW    = 5'b10011;
+parameter SB    = 5'b10100;
+parameter SW    = 5'b10101;
+
+parameter ADD   = 5'b10110;
+parameter SUB   = 5'b10111;
+parameter AND   = 5'b11000;
+parameter OR    = 5'b11001;
+parameter XOR   = 5'b11010;
+parameter SLL   = 5'b11011;
+parameter SRL   = 5'b11100;
+parameter SRA   = 5'b11101;
+parameter SLT   = 5'b11110;
+parameter SLTU  = 5'b11111;
 
 always @*
   if (!cyc)
@@ -59,38 +60,28 @@ always @*
 
 always @(posedge clk) begin
   rs2_num <= inst[13:11];  // Value doesn't matter on first cycle.
+  // TODO: TRAP
   if (!cyc) begin
-    op <= {inst[15:14], inst[7:6], inst[2:0]};
+    op <= inst[4:0];
     rs1_val <= rf_r;
-    case op([2:0])
-      // U-type
-      LUI, AUIPC: begin
-        rs2_imm_val <= {{6{inst[15]}}, inst[14:5]};
-        rd_num <= {1'b0, inst[4:3]};
+
+    case (inst[4:0])
+      TRAP, MOVI, ADDI, ANDI, ORI, XORI, SLI, SRI, JALR, SLTI, SLTIU, INT:
+        rs2_imm_val <= {{9{inst[15]}}, inst[14:8]};
+      LUI, AUIPC:
+        rs2_imm_val <= {inst[15:8], 8'b0};
+      BZ, BNZ, JAL: begin
+        // Trick from RISC-V to keep most bits aligned with immediates above.
+        rs2_imm_val <= {{8{inst[15]}}, inst[8], inst[14:9], 1'b0};
       end
-      // J-type
-      JAL: begin
-        rs2_imm_val <= {};
-        rd_num <= {1'b0, inst[4:3]};
-      end
-      // B-type
-      BNZ: begin
-        rs2_imm_val <= {}
-        rd_num <= inst[5:3];
-      end
-      default: begin
-        rd_num <= inst[5:3];
-        case (op[4:0])
-          // I-type
-          ADDI, SLTI, SLTIU, ANDI, ORI, XORI, SLI, SRI, JALR:
-            rs2_imm_val <= {{12{inst[15}}, inst[14:11]};
-          // S-type
-          SB, SW:
-            rs2_imm_val <= {};
-        endcase
-      end
-      endcase
+      LB, LBU, LW, SB, SW:
+        rs2_imm_val <= {{12{inst[15]}}, inst[14:11]};
+    endcase
   end else begin
+    case (op)
+      ADD, SUB, AND, OR, XOR, SLL, SRL, SRA, SLT, SLTU:
+        rs2_imm_val <= rf_r;
+    endcase
   end
 end
 
