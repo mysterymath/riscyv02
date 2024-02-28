1eca244e284c373b1dc74c6dc4e660a89e3c1477
Daniel Thornburgh <mysterymath@gmail.com>
2024-02-27 16:52:28 -0800

    Expose incremented PC to outside; always write to PC

--- fetch.v
+++ fetch.v
@@ -2,10 +2,8 @@ module fetch(
   clk, cyc, data,
   addr, inst, non_predicted_pc,
   pc_r,
-  pc_w,
-  pc_w_en);
-
-// TODO: Move the program counter in here.
+  pc_r_next,
+  pc_w);
 
 input clk;
 input cyc;
@@ -16,12 +14,46 @@ output reg [15:0] inst;
 output reg [15:1] non_predicted_pc;
 
 input [15:1] pc_r;
+input [15:1] pc_r_next;
 output [15:1] pc_w;
-output pc_w_en;
 
 assign addr = {pc_r, 1'b0};
 
-`include "opcodes.vi"
+parameter TRAP  = 5'b00000;
+parameter MOVI  = 5'b00001;
+parameter ADDI  = 5'b00010;
+parameter ANDI  = 5'b00011;
+parameter ORI   = 5'b00100;
+parameter XORI  = 5'b00101;
+parameter SLI   = 5'b00110;
+parameter SRI   = 5'b00111;
+parameter JALR  = 5'b01000;
+parameter SLTI  = 5'b01001;
+parameter SLTIU = 5'b01010;
+parameter LUI   = 5'b01011;
+parameter AUIPC = 5'b01100;
+parameter BZ    = 5'b01101;
+parameter BNZ   = 5'b01110;
+parameter JAL   = 5'b01111;
+parameter INT   = 5'b10000;
+
+parameter LB    = 5'b10001;
+parameter LBU   = 5'b10010;
+parameter LW    = 5'b10011;
+parameter SB    = 5'b10100;
+parameter SW    = 5'b10101;
+
+parameter ADD   = 5'b10110;
+parameter SUB   = 5'b10111;
+parameter AND   = 5'b11000;
+parameter OR    = 5'b11001;
+parameter XOR   = 5'b11010;
+parameter SLL   = 5'b11011;
+parameter SRL   = 5'b11100;
+parameter SRA   = 5'b11101;
+parameter SLT   = 5'b11110;
+parameter SLTU  = 5'b11111;
+
 
 // Only valid on second cycle.
 wire [15:1] branch_offset;
@@ -42,18 +74,15 @@ wire [15:1] branch_target;
 // yosys should make a 4-bit carry lookahead adder for us.
 assign branch_target = pc_r + branch_offset;
 
-// Only valid on second cycle.
-assign pc_w_en = branch_predicted && cyc;
-assign pc_w = branch_target;
+assign pc_w = (cyc && branch_predicted) ? branch_target : pc_r_next;
 
 always @(posedge clk)
   if (!cyc)
     inst[7:0] <= data;
   else begin
     inst[15:8] <= data;
-    // TODO: Bum the increment off of the program counter.
     if (branch_predicted)
-      non_predicted_pc <= pc_r + 1;
+      non_predicted_pc <= pc_r_next;
     else
       non_predicted_pc <= branch_target;
   end
--- pc.v
+++ pc.v
@@ -1,33 +1,29 @@
-module pc(clk, n_reset, inc_en, w_en, w, r);
+module pc(clk, n_reset, w, r, r_next);
 input clk;
 input n_reset;
-input inc_en;
-input w_en;
 input [15:1] w;
 output reg [15:1] r;
+output [15:1] r_next;
 
 wire [15:1] c_out;
-wire [15:1] inc;
 
 wire lookahead;
 genvar i;
 generate
-  HAX1 inc_1(r[1], 1, inc[1], c_out[1]);
+  HAX1 inc_1(r[1], 1, r_next[1], c_out[1]);
   for (i = 2; i < 9; i = i + 1)
-    HAX1 inc_i(r[i], c_out[i-1], inc[i], c_out[i]);
+    HAX1 inc_i(r[i], c_out[i-1], r_next[i], c_out[i]);
   assign lookahead = r[1] & r[2] & r[3] & r[4] & r[5] & r[6] & r[7] & r[8];
-  HAX1 inc_9(r[9], lookahead, inc[9], c_out[9]);
+  HAX1 inc_9(r[9], lookahead, r_next[9], c_out[9]);
   for (i = 10; i < 16; i = i + 1)
-    HAX1 inc_i(r[i], c_out[i-1], inc[i], c_out[i]);
+    HAX1 inc_i(r[i], c_out[i-1], r_next[i], c_out[i]);
 endgenerate
 
 always @(posedge clk)
   if (!n_reset)
     r <= 16'b0;
-  else if (w_en)
+  else
     r <= w;
-  else if (inc_en)
-    r <= inc;
 
 endmodule
 
