8e6bb73f381967139ea6a065c4aeb9ef0ab2ca72
Daniel Thornburgh <mysterymath@gmail.com>
2024-03-02 17:40:31 -0800

    Muxes are inverting and S picks A

--- rf.v
+++ rf.v
@@ -30,23 +30,27 @@ endgenerate
 
 wire [15:0] r1_0_r[3:0];
 wire [15:0] r1_1_r[1:0];
+wire [15:0] r1_2_r;
 generate
   for (i = 0; i < 4; i++)
-    mux #(16) r1_0(r_r[i], r_r[i+1], r1_num[0], r1_0_r[i]);
+    mux #(16) r1_0(r_r[i+1], r_r[i], r1_num[0], r1_0_r[i]);
   for (i = 0; i < 2; i++)
-    mux #(16) r1_1(r1_0_r[i], r1_0_r[i+1], r1_num[1], r1_1_r[i]);
-  mux #(16) r1_2(r1_1_r[0], r1_1_r[1], r1_num[2], r1);
+    mux #(16) r1_1(r1_0_r[i+1], r1_0_r[i], r1_num[1], r1_1_r[i]);
+  mux #(16) r1_2(r1_1_r[1], r1_1_r[0], r1_num[2], r1_2_r);
 endgenerate;
+assign r1 = ~r1_2_r;
 
 wire [15:0] r2_0_r[3:0];
 wire [15:0] r2_1_r[1:0];
+wire [15:0] r2_2_r;
 generate
   for (i = 0; i < 4; i++)
-    mux #(16) r2_0(r_r[i], r_r[i+1], r2_num[0], r2_0_r[i]);
+    mux #(16) r2_0(r_r[i+1], r_r[i], r2_num[0], r2_0_r[i]);
   for (i = 0; i < 2; i++)
-    mux #(16) r2_1(r2_0_r[i], r2_0_r[i+1], r2_num[1], r2_1_r[i]);
-  mux #(16) r2_2(r2_1_r[0], r2_1_r[1], r2_num[2], r2);
+    mux #(16) r2_1(r2_0_r[i+1], r2_0_r[i], r2_num[1], r2_1_r[i]);
+  mux #(16) r2_2(r2_1_r[1], r2_1_r[0], r2_num[2], r1_2_r);
 endgenerate;
+assign r2 = ~r2_2_r;
 
 endmodule
 
